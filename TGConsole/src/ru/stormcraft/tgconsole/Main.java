fa75ac386c1ab3bf85008bcd440ecb29328bb077
Azim-D3Tm <figaRobot@yandex.ru>
2017-09-06 15:33:00 +0300

    0.10 update

--- TGConsole/src/ru/stormcraft/tgconsole/Main.java
+++ TGConsole/src/ru/stormcraft/tgconsole/Main.java
@@ -11,6 +11,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -304,6 +305,8 @@ public class Main extends JavaPlugin implements Listener {
 		}
 		return result;
 	}
+	
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(args.length<1){
