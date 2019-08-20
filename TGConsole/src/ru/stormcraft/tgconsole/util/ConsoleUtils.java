739555fc44503760a2c9d116adf3ad81042daee1
Sander Kolman <sanderkolman@gmail.com>
2019-08-20 17:37:15 +0200

    schedule dispatching a command

--- TGConsole/src/ru/stormcraft/tgconsole/util/ConsoleUtils.java
+++ TGConsole/src/ru/stormcraft/tgconsole/util/ConsoleUtils.java
@@ -16,43 +16,44 @@ public class ConsoleUtils {
 		ZimmyPrintStream zps = new ZimmyPrintStream(zos);
 		PrintStream def = System.out;
 		System.setOut(zps);
-		try{
-			Bukkit.getServer().dispatchCommand(sender, cmd);
-			Thread.sleep(delay);
-			System.setOut(def);
-			String result = " " + Main.locale.get("commandOutput") + " `";
-			ArrayList<String> res = zps.getData();
-			for(String line:res) {
-				result = result+System.lineSeparator();
-				result = result+line;
+		try {
+			boolean success = Bukkit.getScheduler().callSyncMethod( this, () -> Bukkit.getServer().dispatchCommand( sender, command ) ).get();
+			if(success) {
+				Thread.sleep(delay);
+				System.setOut(def);
+				String result = " " + Main.locale.get("commandOutput") + " `";
+				ArrayList<String> res = zps.getData();
+				for(String line:res) {
+					result = result+System.lineSeparator();
+					result = result+line;
+				}
+				zps.close();
+				result = result.replaceAll("[\\[;0-9]+m", " ");
+				ArrayList<String> newStrings = new ArrayList<String>();
+				while (result.length() > 4001){
+					newStrings.add(result.substring(0, 4000));
+					result = "`" + result.substring(4000);
+				}
+				if (result.length() < 4002) {
+					newStrings.add(result);
+				}
+
+				for (String part : newStrings){
+					part = part + "`";
+					SendMessage message = new SendMessage()
+					.setChatId(chat_id)
+					.setText(part).enableMarkdown(true);
+					bot.execute(message);
+					Thread.sleep(500);
+				}
+
+				Main.clearLog();
 			}
-			zps.close();
-			result = result.replaceAll("[\\[;0-9]+m", " ");
-			ArrayList<String> newStrings = new ArrayList<String>();
-			while (result.length() > 4001){
-				newStrings.add(result.substring(0, 4000));
-				result = "`" + result.substring(4000);
-			}
-			if (result.length() < 4002) {
-				newStrings.add(result);
-			}
-		
-			for (String part : newStrings){
-				part = part + "`";
-				SendMessage message = new SendMessage()
-				.setChatId(chat_id)
-				.setText(part).enableMarkdown(true);
-				bot.execute(message);
-				Thread.sleep(500);
-			}
-			
-			Main.clearLog();
-		
-		}catch(InterruptedException|SecurityException|TelegramApiException e){
+		} catch(InterruptedException|SecurityException|TelegramApiException e) {
 			e.printStackTrace();
 			try{
 				zps.close();
-			}catch(Exception ignored){ }
+			} catch(Exception ignored){ }
 		}
 	}
 }
